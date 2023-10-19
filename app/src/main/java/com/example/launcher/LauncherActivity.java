89736627c71f56c30e3bed84787c6d5ad04f81bb
Donocuica <donocuica1@.com>
2023-10-19 16:52:59 -0500

    UNION

--- app/src/main/java/com/example/launcher/LauncherActivity.java
+++ app/src/main/java/com/example/launcher/LauncherActivity.java
@@ -1,14 +1,27 @@
 package com.example.launcher;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.OnbordingActivity;
 
 public class LauncherActivity extends AppCompatActivity {
+    private static final String TAG = LauncherActivity.class.getSimpleName();
+    private Activity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        activity = LauncherActivity.this;
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(LauncherActivity.this, OnbordingActivity.class));
+            finish();
+        }, 1000);
     }
 }
